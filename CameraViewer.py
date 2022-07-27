07c807a95fe381ebafc48de9b12997e24be1edb1
Oculallen <morgan.allen@me.com>
2022-07-27 15:46:49 +0100

    amongus

--- CameraViewer.py
+++ CameraViewer.py
@@ -1,31 +1,44 @@
 import cv2 as cv
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 
 class MyVideoCapture:
     def __init__(self, source: int=0):
         self.source = source
-        self.vid = cv.VideoCapture(source)
-        ret, frame = self.vid.read()
-        res_frame = self.resize(frame)
-        if not self.vid.isOpened():
-            raise ValueError("Unable to open Video Source", source)
+        self.vid = PiCamera()
+        self.rawcap = PiRGBArray(self.vid)
+        # ret, frame = self.vid.read()
+        # res_frame = self.resize(frame)
+        # if not self.vid.isOpened():
+        #     raise ValueError("Unable to open Video Source", source)
 
-        self.width = res_frame.shape[1]
-        self.height = res_frame.shape[0]
+        time.sleep(0.1)
+
+        self.vid.capture(self.rawcap, format="bgr")
+        image = self.rawcap.array
+
+        self.width = image.shape[1]
+        self.height = image.shape[0]
         self.color = cv.COLOR_BGR2RGB
     
-    def __del__(self):
-        if self.vid.isOpened():
-            self.vid.release()
+    # def __del__(self):
+    #     if self.vid.isOpened():
+    #         self.vid.release()
 
     def get_frame(self):
-        if self.vid.isOpened():
-            ret, frame = self.vid.read()
-            if ret:
-                return (ret, cv.cvtColor(self.resize(frame), self.color))
-            else:
-                return (ret, None)
-        else:
-            return (False, None)
+        # if self.vid.isOpened():
+        #     ret, frame = self.vid.read()
+        #     if ret:
+        #         return (ret, cv.cvtColor(self.resize(frame), self.color))
+        #     else:
+        #         return (ret, None)
+        # else:
+        #     return (False, None)
+        self.vid.capture(self.rawcap, format="bgr")
+        image = self.rawcap.array
+
+        return True, cv.cvtColor(self.resize(image), self.color)
 
     def resize(self, frame, width: int=1250):
         hFactor = width/frame.shape[1]
